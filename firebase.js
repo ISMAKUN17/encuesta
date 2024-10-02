7ca6bda726ee643aa0fb05bea42af914dd596296
ISMAKUN17 <PROYECTOSISMAEL17@GMAIL.COM>
2024-10-02 14:53:54 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
-// firebase.js
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDUirwoSQgvcMZn7uLjKMAxlTA15Jkl2BM",
-    authDomain: "ismakun-bf0fa.firebaseapp.com",
-    projectId: "ismakun-bf0fa",
-    storageBucket: "ismakun-bf0fa.appspot.com",
-    messagingSenderId: "1028877703945",
-    appId: "1:1028877703945:web:331de48acbcdf2234a51d8"
+  apiKey: "AIzaSyDUirwoSQgvcMZn7uLjKMAxlTA15Jkl2BM",
+  authDomain: "ismakun-bf0fa.firebaseapp.com",
+  projectId: "ismakun-bf0fa",
+  storageBucket: "ismakun-bf0fa.appspot.com",
+  messagingSenderId: "1028877703945",
+  appId: "1:1028877703945:web:331de48acbcdf2234a51d8"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
