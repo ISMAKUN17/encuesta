774fe560384f5ff8e8bd8b18211dbef116198974
ISMAKUN17 <PROYECTOSISMAEL17@GMAIL.COM>
2024-10-02 14:43:10 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
-// Importa las funciones necesarias del SDK de Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+// firebase.js
 
-// Tu configuración de Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDUirwoSQgvcMZn7uLjKMAxlTA15Jkl2BM",
-  authDomain: "ismakun-bf0fa.firebaseapp.com",
-  databaseURL: "https://ismakun-bf0fa-default-rtdb.firebaseio.com",
-  projectId: "ismakun-bf0fa",
-  storageBucket: "ismakun-bf0fa.appspot.com",
-  messagingSenderId: "1028877703945",
-  appId: "1:1028877703945:web:331de48acbcdf2234a51d8"
+    apiKey: "AIzaSyDUirwoSQgvcMZn7uLjKMAxlTA15Jkl2BM",
+    authDomain: "ismakun-bf0fa.firebaseapp.com",
+    projectId: "ismakun-bf0fa",
+    storageBucket: "ismakun-bf0fa.appspot.com",
+    messagingSenderId: "1028877703945",
+    appId: "1:1028877703945:web:331de48acbcdf2234a51d8"
 };
 
-// Inicializa Firebase y Firestore
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
